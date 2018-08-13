5c72f23aadd76cc0222881e9042a22b3d2bd38c4
jack <chengjunjie.jack@gmail.com>
2018-08-13 16:21:33 +0800

    修改小bug

--- cpserver/route/weixin.php
+++ cpserver/route/weixin.php
@@ -4,7 +4,7 @@
  */
 use think\facade\Route;
 
-Route::get('wx','admin/weiXin.CheckServer/checkServer');
+Route::get('wx','admin/weixin.CheckServer/checkServer');
 // 微信服务器验证接口
 // 发送群发模板消息接口
 // Route::any('manage/weixin/sendalltemplatemessage','admin/weixin.TemplateMessage/SendAllTemplateMessage');
