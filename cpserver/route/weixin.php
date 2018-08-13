e9b725037365e9ab3f16ce96a8e0f76ba48d7052
jack <chengjunjie.jack@gmail.com>
2018-08-13 15:54:37 +0800

    修改GetJsapi的文件名

--- cpserver/route/weixin.php
+++ cpserver/route/weixin.php
@@ -9,6 +9,6 @@ Route::get('wx','admin/weiXin.CheckServer/checkServer');
 // 发送群发模板消息接口
 // Route::any('manage/weixin/sendalltemplatemessage','admin/weixin.TemplateMessage/SendAllTemplateMessage');
 // 获取jsapi签名
-Route::any('ps/weixin/getjsapi','admin/weixin.GetJsApi/getSignature');
+Route::any('ps/weixin/getjsapi','admin/weixin.GetJsapi/getSignature');
 // 刷新jsapi签名
-Route::get('ps/weixin/flashjsapi','admin/weixin.GetJsApi/getJsApiFromWeixin');
+Route::get('ps/weixin/flashjsapi','admin/weixin.GetJsapi/getJsApiFromWeixin');
