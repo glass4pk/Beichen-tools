2d2505d1fdfb8f7b431ff61b45966c4c25274776
jack <chengjunjie.jack@gmail.com>
2018-08-14 15:19:29 +0800

    更新thinkphp框架

--- cpserver/config/database.php
+++ cpserver/config/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => 'dev.yes-go.cn',
     // 数据库名
     'database'        => 'cpmapping',
     // 用户名
