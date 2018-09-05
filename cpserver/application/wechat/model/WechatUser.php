589872e8b8700a77ec024743e5396f34c56a176c
Clelo4 <jack0000davis@gmail.com>
2018-09-06 01:39:14 +0800

    同步

--- cpserver/application/wechat/model/WechatUser.php
+++ cpserver/application/wechat/model/WechatUser.php
@@ -23,14 +23,16 @@ class WechatUser extends Model
                 $paramArray['last_change_timestamp'] = strtotime('now');
                 // 刷新数据库
                 $isOk = $this->where(['openid' => $paramArray['openid']])->update($paramArray);
+                var_dump( "isOk == true:" . $isOk);
             } else {
                 // 新用户插入基类
                 $paramArray['create_timestamp'] = strtotime('now');
                 $paramArray['last_change_timestamp'] = $paramArray['create_timestamp'];
                 $isOk = $this->insert($paramArray);
+                var_dump("insert" . $isOk);
             }
         } catch(Exception $e) {
-            // var_dump($e);
+            //var_dump($e);
             $isOk = false;
         } finally {
             return $isOk;
